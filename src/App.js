c5441cdd84723204ed4aca12a091d7e137e052d2
Tamara Luzija <tamara.luzija1@gmail.com>
2020-05-17 11:34:55 +0200

    learn fetching data with useEffect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import "./App.css";
 import ClassCounter from "./ComponentsHooks/ClassCounter";
 import HookCounterTwo from "./ComponentsHooks/HookCounterTwo";
-import IntervalHookCounter from "./ComponentsHooks/IntervalHookCounter";
+import DataFetching from "./ComponentsHooks/DataFetching";
 
 function App() {
   return (
     <div className="App">
-      <IntervalHookCounter />
+      <DataFetching />
     </div>
   );
 }
