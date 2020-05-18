15c79a51a7e7e868930e238b91ae847176f0785e
Tamara Luzija <tamara.luzija1@gmail.com>
2020-05-18 17:35:13 +0200

    DataFetching-useReducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
-import ClassCounter from "./ComponentsHooks/ClassCounter";
-import HookCounterTwo from "./ComponentsHooks/HookCounterTwo";
-import ReduxCounterThree from "./ComponentsHooks/ReduxCounterThree";
+import DataFetchingTwo from "./ComponentsHooks/DataFetchingTwo";
 
 // export const UserContext = React.createContext();
 // export const ChannelContext = React.createContext();
 
+// export const CountContext = React.createContext();
+
+// const initialState = 0;
+// const reducer = (state, action) => {
+//   switch (action) {
+//     case "increment":
+//       return state + 1;
+//     case "decrement":
+//       return state - 1;
+//     case "reset":
+//       return initialState;
+//     default:
+//       return state;
+//   }
+// };
+
 function App() {
+  // const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      <ReduxCounterThree />
-    </div>
+    <DataFetchingTwo />
+    // <CountContext.Provider
+    //   value={{ countState: count, countDispatch: dispatch }}
+    // >
+    //   <div className="App">
+    //     Count - {count}
+    //     <ComponentA />
+    //     <ComponentB />
+    //     <ComponentC />
+    //   </div>
+    // </CountContext.Provider>
   );
 }
 
